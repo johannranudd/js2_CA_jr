02ed1dc1d9af42186492b5e05db66a1bb2824592
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-09-16 20:24:09 +0200

    sidebar and mobilebar working. next: make contacts transitions

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,13 +2,23 @@ const feedAndContactsContaier = document.querySelector(
   '.feed-and-contacts-container'
 );
 const sidebar = document.querySelector('.sidebar');
+const menuBtn = document.querySelector('.menu-btn');
+
+// MOBILE
+
+menuBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
+});
+
+// > 500
 
 window.addEventListener('resize', adjustForSidebar);
 
 function adjustForSidebar() {
-  const sidebarWidth = sidebar.getBoundingClientRect().width;
   if (window.innerWidth >= 500) {
+    const sidebarWidth = sidebar.getBoundingClientRect().width;
     feedAndContactsContaier.style.marginLeft = `${sidebarWidth}px`;
+    sidebar.classList.remove('show-sidebar');
     console.log('removing');
   } else {
     feedAndContactsContaier.style.marginLeft = `0px`;
