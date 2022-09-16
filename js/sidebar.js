70527230a7ade0da37e78fac27693d208caf5bb5
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-09-17 01:45:51 +0200

    contacts need to go through again

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,6 +5,8 @@ const sidebar = document.querySelector('.sidebar');
 const menuBtn = document.querySelector('.menu-btn');
 const contactsBtn = document.querySelector('.contacts-btn');
 const contacts = document.querySelector('.contacts');
+const listOfContacts = document.querySelector('.list-of-contacts');
+const mobilebar = document.querySelector('.mobilebar');
 
 // MOBILE
 
@@ -20,14 +22,44 @@ contactsBtn.addEventListener('click', () => {
 window.addEventListener('resize', adjustForSidebar);
 
 function adjustForSidebar() {
+  const sidebarWidth = sidebar.getBoundingClientRect().width;
+  const contactWidth = contacts.getBoundingClientRect().width;
+  console.log(contactWidth);
   if (window.innerWidth >= 500) {
-    const sidebarWidth = sidebar.getBoundingClientRect().width;
     feedAndContactsContaier.style.marginLeft = `${sidebarWidth}px`;
     sidebar.classList.remove('show-sidebar');
     console.log('removing');
+  } else if (window.innerWidth > 1024) {
+    feedAndContactsContaier.style.marginRight = `${contactWidth}px`;
   } else {
     feedAndContactsContaier.style.marginLeft = `0px`;
+    // feedAndContactsContaier.style.marginRight = `0px`;
+    // contacts.classList.remove('show-contacts');
     console.log('NOOOOt removing');
   }
 }
 adjustForSidebar();
+
+// let lastScroll = 0;
+// window.addEventListener('scroll', (e) => {
+//   const listRect = contacts.getBoundingClientRect();
+//   const mobilebarHeight = mobilebar.getBoundingClientRect().height;
+
+//   if (window.pageYOffset > lastScroll) {
+//     console.log('down');
+//     if (listRect.bottom > window.pageYOffset * 2) {
+//     } else {
+//       contacts.style.position = 'fixed';
+//       contacts.style.bottom = `${mobilebarHeight}px`;
+//     }
+//     lastScroll = window.pageYOffset;
+//   } else if (window.pageYOffset < lastScroll) {
+//     if (
+//       listRect.height > window.pageYOffset &&
+//       contacts.className.includes('show-contacts')
+//     ) {
+//       // console.log('erdgrf');
+//     }
+//     lastScroll = window.pageYOffset;
+//   }
+// });
