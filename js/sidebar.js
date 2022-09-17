e3e091f06e5159ad7894104ed39835361fae3c0b
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-09-18 01:32:46 +0200

    contacts closer to finish

--- js/sidebar.js
+++ js/sidebar.js
@@ -3,18 +3,24 @@ const feedAndContactsContaier = document.querySelector(
 );
 const sidebar = document.querySelector('.sidebar');
 const menuBtn = document.querySelector('.menu-btn');
-const contactsBtn = document.querySelector('.contacts-btn');
+const contactBtns = document.querySelectorAll('.contacts-btn');
 const contacts = document.querySelector('.contacts');
 const listOfContacts = document.querySelector('.list-of-contacts');
 const mobilebar = document.querySelector('.mobilebar');
+const mainContainer = document.querySelector('.main-container');
 
 // MOBILE
 
 menuBtn.addEventListener('click', () => {
   sidebar.classList.toggle('show-sidebar');
 });
-contactsBtn.addEventListener('click', () => {
-  contacts.classList.toggle('show-contacts');
+contactBtns.forEach((btn) => {
+  // if (window.innerWidth < 1024) {
+  // }
+  btn.addEventListener('click', () => {
+    contacts.classList.toggle('show-contacts');
+    adjustForSidebar();
+  });
 });
 
 // > 500
@@ -23,22 +29,33 @@ window.addEventListener('resize', adjustForSidebar);
 
 function adjustForSidebar() {
   const sidebarWidth = sidebar.getBoundingClientRect().width;
-  const contactWidth = contacts.getBoundingClientRect().width;
-  console.log(contactWidth);
+
   if (window.innerWidth >= 500) {
     feedAndContactsContaier.style.marginLeft = `${sidebarWidth}px`;
     sidebar.classList.remove('show-sidebar');
-    console.log('removing');
-  } else if (window.innerWidth > 1024) {
-    feedAndContactsContaier.style.marginRight = `${contactWidth}px`;
   } else {
     feedAndContactsContaier.style.marginLeft = `0px`;
-    // feedAndContactsContaier.style.marginRight = `0px`;
-    // contacts.classList.remove('show-contacts');
-    console.log('NOOOOt removing');
   }
+  adjustForContacts();
+  // !test
+  // const mainContainerRect = mainContainer.getBoundingClientRect();
+  // console.log(mainContainerRect.width);
+  // if (mainContainerRect.width >= 1280){
+
+  // }
+  // !test
 }
 adjustForSidebar();
+
+function adjustForContacts() {
+  // contacts.className.includes('show-contacts') &&
+  const contactWidth = contacts.getBoundingClientRect().width;
+  if (window.innerWidth >= 1024) {
+    feedAndContactsContaier.style.marginRight = `${contactWidth}px`;
+  } else {
+    feedAndContactsContaier.style.marginRight = `0px`;
+  }
+}
 
 // let lastScroll = 0;
 // window.addEventListener('scroll', (e) => {
