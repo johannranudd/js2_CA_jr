29c3060f48f67fa8aaaaa431f6f3fe53998e4139
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-09-23 17:28:01 +0200

    dev branch. custom-file-uploader works in post component. needs styling. image uploads to client. Next: test with different images, submit form with an object that fits the POST method for the api

--- js/post.js
+++ js/post.js
@@ -1,8 +1,10 @@
 const textareaPost = document.querySelector('.post-textarea');
 const feedContainer = document.querySelector('.feed');
 const homeComponent = document.querySelector('.home-component');
-const addImageContainer = document.querySelector('.add-image-container');
-const addImageBtn = document.querySelector('.add-image-btn');
+const displayImageContainer = document.querySelector(
+  '.display-image-container'
+);
+const uploadImgeInput = document.querySelector('.upload-img-input');
 
 window.addEventListener('resize', () => {
   const feedRect = feedContainer.getBoundingClientRect();
@@ -16,23 +18,18 @@ textareaPost.addEventListener('keyup', (e) => {
   textareaPost.style.height = `${e.target.scrollHeight}px`;
 });
 
-// addImageContainer
-
-addImageBtn.addEventListener('click', () => {
-  console.log('open');
+// upload image
+uploadImgeInput.addEventListener('change', () => {
+  // console.log(uploadImgeInput.files);
+  const reader = new FileReader();
+  reader.onload = function () {
+    const img = new Image();
+    img.src = reader.result;
+    const alt = document.createAttribute('alt');
+    alt.value = 'Your uploaded image';
+    img.setAttributeNode(alt);
+    displayImageContainer.appendChild(img);
+    console.log(displayImageContainer);
+  };
+  reader.readAsDataURL(uploadImgeInput.files[0]);
 });
-
-// !test
-// imgInput.addEventListener('change', () => {
-//   // console.log(imgInput.files);
-//   const reader = new FileReader();
-//   reader.onload = function () {
-//     const img = new Image();
-//     img.src = reader.result;
-//     imgDisplayer.style.backgroundImage = `url(${img.src})`;
-//   };
-//   reader.readAsDataURL(imgInput.files[0]);
-//   imgDisplayer.style.height = '300px';
-//   imgDisplayer.style.width = '300px';
-// });
-// !test
