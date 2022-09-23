df21d45411eaf301ae941b333a7156a57715dc39
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-09-23 07:57:10 +0200

    dev branch, markup is good for post component. needs styling but will do that later. Next: figure out wether to use input or btn for uploading image, then make a post with the form

--- js/post.js
+++ js/post.js
@@ -1,6 +1,8 @@
 const textareaPost = document.querySelector('.post-textarea');
 const feedContainer = document.querySelector('.feed');
 const homeComponent = document.querySelector('.home-component');
+const addImageContainer = document.querySelector('.add-image-container');
+const addImageBtn = document.querySelector('.add-image-btn');
 
 window.addEventListener('resize', () => {
   const feedRect = feedContainer.getBoundingClientRect();
@@ -8,3 +10,29 @@ window.addEventListener('resize', () => {
 });
 
 // eventlistener for textarea
+
+textareaPost.addEventListener('keyup', (e) => {
+  textareaPost.style.height = 'auto';
+  textareaPost.style.height = `${e.target.scrollHeight}px`;
+});
+
+// addImageContainer
+
+addImageBtn.addEventListener('click', () => {
+  console.log('open');
+});
+
+// !test
+// imgInput.addEventListener('change', () => {
+//   // console.log(imgInput.files);
+//   const reader = new FileReader();
+//   reader.onload = function () {
+//     const img = new Image();
+//     img.src = reader.result;
+//     imgDisplayer.style.backgroundImage = `url(${img.src})`;
+//   };
+//   reader.readAsDataURL(imgInput.files[0]);
+//   imgDisplayer.style.height = '300px';
+//   imgDisplayer.style.width = '300px';
+// });
+// !test
